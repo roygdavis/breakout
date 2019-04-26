9b6d0a3f826a790220357551dec9d481334c88d9
Roy Davis <roy.davis@mountainwarehouse.com>
2019-04-26 15:34:45 +0100

    Balls and bricks

--- src/ui-components/Ball.tsx
+++ src/ui-components/Ball.tsx
@@ -17,38 +17,54 @@ export interface BallState {
     ballGoingUp: boolean
     ballGoingLeft: boolean;
     speed: number;
-    paddleXPos: number;
 }
 
 class Ball extends React.Component<BallProps, BallState> {
-    timer: NodeJS.Timeout;
+    timer: NodeJS.Timeout | undefined;
 
     constructor(props: BallProps) {
         super(props);
         this.state = { ...this.props };
+    }
+
+    componentDidMount() {
         this.timer = setInterval(() => this.moveBall(), this.state.speed);
     }
 
+    componentWillUnmount() {
+        if (this.timer) clearInterval(this.timer);
+    }
+
     moveBall = () => {
-        let { xPos, yPos, ballGoingUp, ballGoingLeft, paddleXPos } = this.state;
+        let { xPos, yPos, ballGoingUp, ballGoingLeft } = this.state;
+        let ballLost = false;
         // set this ball position
         if (xPos > 1000) ballGoingLeft = true;
         if (xPos < 0) ballGoingLeft = false;
         if (yPos < 0) ballGoingUp = false;
         if (yPos > 480) {
-            if (xPos > (paddleXPos - 50)
-                && xPos < (paddleXPos + 50)) {
-                ballGoingUp = true;
-            }
-            else {
-                this.props.ballLostEvent(this);
-            }
+            ballLost = !this.didBallHitPaddle();
+            ballGoingUp = !ballLost;
         }
         // move
         yPos += ballGoingUp ? -10 : 10;
         xPos += ballGoingLeft ? -10 : 10;
 
-        this.setState({ xPos, yPos, ballGoingUp, ballGoingLeft, paddleXPos });
+        this.setState({ xPos, yPos, ballGoingUp, ballGoingLeft });
+
+        if (ballLost) this.props.ballLostEvent(this);
+    }
+
+    didBallHitPaddle = () => {
+        const { xPos } = this.state;
+        const { paddleXPos } = this.props;
+        if (xPos > (paddleXPos - 50) && xPos < (paddleXPos + 50)) {
+            console.log(`Collision detection - ID = ${this.props.id}\t Ball X = ${xPos}\t Paddle X = ${paddleXPos}`);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     render() {
--- src/ui-components/Canvas.tsx
+++ src/ui-components/Canvas.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Paddle from './Paddle';
 import Ball, { BallProps } from './Ball';
+import Brick, { BrickProps } from './Brick';
+import { array } from 'prop-types';
 
 export interface CanvasProps {
 }
@@ -8,42 +10,68 @@ export interface CanvasProps {
 export interface CanvasState {
     paddleXPos: number,
     balls: Array<BallProps>,
+    bricks: Array<BrickProps>,
     gameOver: boolean,
     intervalId: any
 }
 
 class Canvas extends React.Component<CanvasProps, CanvasState> {
     initialPaddleXPos = 500;
+    numberOfBalls = 10;
+    brickPower = 10;
 
     constructor(props: CanvasProps) {
         super(props);
+        const balls = this.generateBalls();
+        const bricks = this.generateBricks(balls);
+
         this.state = {
             paddleXPos: this.initialPaddleXPos,
-            balls: this.generateBalls(),
+            balls: balls,
             gameOver: false,
-            intervalId: null
+            intervalId: null,
+            bricks: bricks
         };
     }
 
     componentDidMount() {
         window.onkeydown = this.handleKeyDown;
+        window.onmousemove = this.handleMouseMove;
     }
 
     generateBalls = () => {
         let balls = new Array<BallProps>();
-        for (let index = 0; index < 10; index++) {
+        for (let index = 0; index < this.numberOfBalls; index++) {
             balls.push({
                 xPos: Math.floor(Math.random() * 1000),
                 yPos: 100,
                 ballGoingUp: Math.round(Math.random()) === 1,
                 ballGoingLeft: Math.round(Math.random()) === 1,
-                speed: Math.random() * 1000,
+                speed: Math.random() * 100,
                 paddleXPos: this.initialPaddleXPos,
                 ballLostEvent: this.handleDroppedBall,
                 id: index
             });
         }
         return balls;
+    }
+
+    generateBricks = (balls: Array<BallProps>) => {
+        let bricks = new Array<BrickProps>();
+        let id = 0;
+        for (let y = 0; y < 100; y += 5) {
+            for (let x = 0; x < 1000; x += 100) {
+                bricks.push({
+                    xPos: x,
+                    yPos: y,
+                    power: this.brickPower,
+                    balls: balls,
+                    id: id
+                });
+                id++;
+            }
+        }
+        return bricks;
     }
 
     handleKeyDown = (ev: KeyboardEvent) => {
@@ -62,19 +90,18 @@ class Canvas extends React.Component<CanvasProps, CanvasState> {
         this.setState({ paddleXPos });
     }
 
+    handleMouseMove = (ev: MouseEvent) => {
+        //console.log(ev.clientX);
+        let { paddleXPos } = this.state;
+        paddleXPos = ev.clientX;
+        this.setState({ paddleXPos });
+    }
+
     handleDroppedBall = (ball: Ball) => {
-        console.log("lost a ball");
+        //console.log("lost a ball");
         const { balls } = this.state;
         this.setState({ balls: balls.filter(f => f.id !== ball.props.id) });
     }
-
-    draw = () => {
-        //const { gameOver } = this.state;
-        //if (!gameOver) { this.moveBall() } else { this.gameOver(); }
-
-    }
-
-
 
     gameOver = () => {
         const { intervalId } = this.state;
@@ -83,11 +110,12 @@ class Canvas extends React.Component<CanvasProps, CanvasState> {
     }
 
     render() {
-        const { paddleXPos, balls } = this.state;
+        const { paddleXPos, balls, bricks } = this.state;
         return (
             <svg viewBox="0 0 1000 800" xmlns="http://www.w3.org/2000/svg">
                 <Paddle xPos={paddleXPos}></Paddle>
-                {balls.map(b => <Ball {...b} paddleXPos={paddleXPos} />)}
+                {balls.map(b => <Ball key={b.id} {...b} paddleXPos={paddleXPos} />)}
+                {bricks.map(k => <Brick key={k.id} {...k}></Brick>)}
             </svg>);
     }
 }
