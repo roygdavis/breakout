2c851bbe597ae16a87ef23611d4cea5056220eff
Roy Davis <roygdavis@gmail.com>
2019-04-26 19:27:02 +0100

    Removed unused helper file

--- src/helpers/helpers.ts
+++
@@ -1,8 +0,0 @@
-/*
-function rnd(min: number, max: number): number {
-    let num = Math.random() * 50;
-    while (num < min && num > max) {
-        num = Math.random();
-    }
-    return num;
-}*/
